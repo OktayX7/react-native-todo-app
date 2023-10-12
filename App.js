20adcaed7275995412e68bf045ab371d350dbdd4
oktay gedik <oktaygedik@oktay-MacBook-Air.local>
2023-10-12 10:43:06 +0300

    initialize project

---
+++ App.js
@@ -0,0 +1,16 @@
+import {StyleSheet, Text, View, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>App</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
